a22b1e92a14f16bed95f050069b8dcd49b21d3d2
Diego Mulet <diegoqmulet@gmail.com>
2021-04-30 16:07:06 -0300

    Commit estrutura

---
+++ Coin.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Coin.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ Coin.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Coin.Domain
+{
+    public class Class1
+    {
+    }
+}
